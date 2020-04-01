77e8c8bd9b870665155a35a62a979eef57e2caab
fedefyco <federicorosario.conticello@yahoo.it>
2020-04-02 00:02:53 +0200

    created registration form, login and logout system

--- django_website/blog/views.py
+++ django_website/blog/views.py
@@ -1,30 +1,32 @@
 from django.shortcuts import render
+from .models import Post
 
 
-posts = [{
-
-    'author': 'Corey Taylor',
-    'title': 'Blog post 1',
-    'content': 'First post content',
-    'date_posted': 'August, 27 2020'
-
-    },
-    {
-    'author': 'Amelia Taylor',
-    'title': 'Blog post 2',
-    'content': 'Second post content',
-    'date_posted': 'August, 28 2020'
-
-    }
-]
+# Sono finiti tutti in models
+# posts = [{
+#
+#     'author': 'Corey Taylor',
+#     'title': 'Blog post 1',
+#     'content': 'First post content',
+#     'date_posted': 'August, 27 2020'
+#
+#     },
+#     {
+#     'author': 'Amelia Taylor',
+#     'title': 'Blog post 2',
+#     'content': 'Second post content',
+#     'date_posted': 'August, 28 2020'
+#
+#     }
+# ]
 
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all() #al posto di 'posts': posts
     }
     return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
 
